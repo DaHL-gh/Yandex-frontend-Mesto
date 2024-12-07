6c282da0df4cbe258e7ebf516435acc50eedd0ee
DaHL <8tima18@gmail.com>
2024-12-07 15:16:43 +0500

    File structure rework

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,6 @@
-import "./index.css"
+import { initialCards } from "./cards.js"
+import "../pages/index.css"
+import "../vendor/normalize.css"
 
 // ---------------------------- FUNCTIONS --------------------------------
 
@@ -90,33 +92,6 @@ function addCard(name, url) {
 
   placesList.append(cardElement);
 }
-
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
 
 initialCards.forEach((item) => addCard(item.name, item.link));
 
