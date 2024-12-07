8df5750ddc77c87259a0861047cf27a70d089d58
DaHL <8tima18@gmail.com>
2024-12-07 11:42:41 +0500

    Input validation for profile eidtor

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,35 @@
+// ---------------------------- FUNCTIONS --------------------------------
+
 function togglePopup(popupClass) {
   document.querySelector(popupClass).classList.toggle("popup_is-opened");
+}
+
+function showInputError(inputElement, errorElement) {
+  inputElement.classList.add("popup__input_type_error");
+  errorElement.textContent = inputElement.validationMessage;
+}
+
+function hideInputError(inputElement, errorElement) {
+  inputElement.classList.remove("popup__input_type_error");
+  errorElement.textContent = "";
+}
+
+function validateForm(formElement, submitButton, ...inputElements) {
+  inputElements.forEach((element) => {
+    if (!element.validity.valid) {
+      showInputError(element, element.nextElementSibling);
+    } else {
+      hideInputError(element, element.nextElementSibling);
+    }
+  });
+
+  if (formElement.checkValidity()) {
+    submitButton.disabled = false;
+    submitButton.classList.remove("popup__button_disabled");
+  } else {
+    submitButton.disabled = true;
+    submitButton.classList.add("popup__button_disabled");
+  }
 }
 
 // ------------------------------ CARDS ----------------------------------
@@ -65,19 +95,24 @@ initialCards.forEach((item) => addCard(item.name, item.link));
 
 // -------------------------- EDIT POP-UP -------------------------------
 
-editForm = document.forms.edit-profile
+editForm = document.forms.namedItem("edit-profile");
 editPopup = document.querySelector(".popup_type_edit");
 editPopup.classList.add("popup_is-animated");
 
 const nameInput = editForm.elements.name;
-const descriptionInput = editForm.elements.description;
+nameInput.addEventListener("input", () => {
+  validateForm(editForm, submitEditButton, nameInput);
+});
+const descInput = editForm.elements.description;
+descInput.addEventListener("input", () => {
+  validateForm(editForm, submitEditButton, descInput);
+});
 
 editProfileButton = document.querySelector(".profile__edit-button");
 editProfileButton.addEventListener("click", () => {
   nameInput.value = document.querySelector(".profile__title").textContent;
-  descriptionInput.value = document.querySelector(
-    ".profile__description"
-  ).textContent;
+  descInput.value = document.querySelector(".profile__description").textContent;
+  validateForm(editForm, submitEditButton, nameInput, descInput)
 
   togglePopup(".popup_type_edit");
 });
@@ -87,17 +122,14 @@ closeEditButton.addEventListener("click", () => {
   togglePopup(".popup_type_edit");
 });
 
-const submitEditButton = editForm.elements.submit-button;
+const submitEditButton = editForm.elements.namedItem("submit-button");
 submitEditButton.addEventListener("click", (event) => {
-  if (!!inputInput.value && !!descriptionInput.value) {
-    event.preventDefault();
+  event.preventDefault();
 
-    document.querySelector(".profile__title").textContent = inputInput.value;
-    document.querySelector(".profile__description").textContent =
-      descriptionInput.value;
+  document.querySelector(".profile__title").textContent = nameInput.value;
+  document.querySelector(".profile__description").textContent = descInput.value;
 
-    togglePopup(".popup_type_edit");
-  }
+  togglePopup(".popup_type_edit");
 });
 
 // -------------------------- ADD POP-UP -------------------------------
